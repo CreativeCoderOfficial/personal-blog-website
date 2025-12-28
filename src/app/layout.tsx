580995cd43b7814fbd44b3ebb9793eab852636de
CreativeCoderOfficial <creativecoder.official@outlook.com>
2025-12-28 19:43:25 +0100

    Redefined layout and theme, to apply custom parts for the admin and general website section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,18 @@
 import "@/app/globals.css";
-import { Inter } from "next/font/google"; // global font
-import Header from "@/components/general/Header";
-import Footer from "@/components/general/Footer";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Maxxed Out",
-  description: "My Portfolio and Blog",
+  description: "My Portfolio",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-main text-text-primary`}>
-        {/* Header appears on every page */}
-        <Header />
-        
-        {/* This is where your page content (Home, Blogs) is injected */}
+      <body className={`${inter.className} antialiased`}>
         {children}
-        
-        {/* Footer appears on every page */}
-        <Footer />
       </body>
     </html>
   );
